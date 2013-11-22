18a5ef1d63770609af3919a8eea576c66aaa477c
pavel.stastny@gmail.com <pavel.stastny@gmail.com>
2013-11-22 11:06:04 +0100

    commit to repo

--- src/main/java/cz/incad/kramerius/k5/k5velocity/RESTHelper.java
+++ src/main/java/cz/incad/kramerius/k5/k5velocity/RESTHelper.java
@@ -3,6 +3,7 @@ package cz.incad.kramerius.k5.k5velocity;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -13,12 +14,16 @@ public class RESTHelper {
 
     public static InputStream inputStream(String urlString) throws IOException {
         URLConnection uc = openConnection(urlString);
+        HttpURLConnection hcon = (HttpURLConnection) uc;
+        hcon.setInstanceFollowRedirects(true);
         return uc.getInputStream();
     }
 
     public static URLConnection openConnection(String urlString) throws MalformedURLException, IOException {
         URL url = new URL(urlString);
         URLConnection uc = url.openConnection();
+        HttpURLConnection hcon = (HttpURLConnection) uc;
+        hcon.setInstanceFollowRedirects(true);
         uc.setReadTimeout(Integer.parseInt("10000"));
         uc.setConnectTimeout(Integer.parseInt("10000"));
         return uc;
